c89865f7121f2eb2ef0e75f05fc07d5699075219
lytves <22356770+lytves@users.noreply.github.com>
2021-02-28 18:14:28 +0100

    set last launches numbers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './assets/SpaceX-Logo.svg';
 import './App.css';
 import {gql, useQuery} from '@apollo/client';
+import {useState} from 'react';
 
 const PAST_LAUNCHES = gql`
   query GetPastLaunces($numLaunches: Int!) {
@@ -18,21 +19,35 @@ const PAST_LAUNCHES = gql`
 `;
 
 function App() {
+    const [numLaunches, setNumLaunches] = useState(10);
     const {loading, error, data} = useQuery(PAST_LAUNCHES, {
         variables: {
-            numLaunches: 10,
+            numLaunches
         },
     });
-
+    const container = {
+        color: "white",
+        backgroundColor: "DodgerBlue",
+        padding: "10px",
+        fontFamily: "Arial"
+    };
+    const buttons =  {
+        padding: 20
+    }
     if (loading) {
         return <p>Loading...</p>;
     }
 
     if (error) {
-        return <p>Oh no!</p>;
+        return <p>Oh no, error!</p>;
     }
 
     return (
+        <div style={container}>
+            <div style={buttons}>
+                <button onClick={() => setNumLaunches(5)}>Show 5</button>
+                <button onClick={() => setNumLaunches(10)}>Show 10</button>
+            </div>
             <ul>
                 <li>
                     <img src={logo} alt="SpaceX" style={{height: 50}}/>
@@ -51,6 +66,7 @@ function App() {
                     </li>
                 ))}
             </ul>
+        </div>
     );
 }
 
